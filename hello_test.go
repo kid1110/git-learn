8631edf679b5d7df7740adb84b7f676ff0696ada
kid <1050109853@qq.com>
2024-02-21 14:38:54 +0800

    feat: update file

--- hello_test.go
+++ hello_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	if ans := Add(1, 2); ans != 3 {
+	if ans := Add(1, 2); ans != 4 {
 		t.Errorf("1 + 2 expected be 3, but %d got", ans)
 	}
 }
