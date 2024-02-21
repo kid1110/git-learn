af89fd704526b6c48f04a9d036a4ad7148a26cfd
kid <1050109853@qq.com>
2024-02-21 14:27:01 +0800

    feat: update file

---
+++ hello_test.go
@@ -0,0 +1,9 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	if ans := Add(1, 2); ans != 3 {
+		t.Errorf("1 + 2 expected be 3, but %d got", ans)
+	}
+}
--- main.go
+++ main.go
@@ -5,3 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 }
+
+func Add(a int, b int) int {
+	return a + b
+}
